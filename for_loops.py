8eae4e56c077382f63d5eb4f99e0225ef218a08d
Kirt Morgan <kirtmorgan@live.com>
2019-04-17 14:43:23 +0100

    Further work on loops now introducing FOR loops and seeing their benifit

---
+++ for_loops.py
@@ -0,0 +1,14 @@
+# For Loops
+# For place_holder in list_object
+# execute this block of code
+# Id List
+# ids_list = [1, 2, 3, 4, 5]
+# for id in ids_list:
+#     print(id)
+
+# Accessing data within a list
+# embedded_data = [[1, 2, 3], [7, 12, 1]]
+# for data_list in embedded_data:
+#     print(data_list)
+#     for data in data_list:
+#         print(data)
