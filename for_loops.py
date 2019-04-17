4e264c19c8479d7b51cf9ffe5f3f991ba87f117c
Kirt Morgan <kirtmorgan@live.com>
2019-04-17 15:18:44 +0100

    Update on the for loops file, working on for loops in more detail

--- for_loops.py
+++ for_loops.py
@@ -1,4 +1,4 @@
-# For Loops
+# FOR Loops
 # For place_holder in list_object
 # execute this block of code
 # Id List
@@ -6,9 +6,25 @@
 # for id in ids_list:
 #     print(id)
 
-# Accessing data within a list
+# Accessing data within a list embedded FOR loops
 # embedded_data = [[1, 2, 3], [7, 12, 1]]
 # for data_list in embedded_data:
 #     print(data_list)
 #     for data in data_list:
 #         print(data)
+
+# Loops for dictionaries
+# accounts = {
+#     1:{'Name': 'Arya','Money':'£10000000'},
+#     2:{'Name': 'Bob','Money':'£10000'},
+#     3:{'Name': 'Sponge','Money':'£500000'}
+# }
+# for key in accounts:
+    # print(accounts[key])
+    # for embedded_keys in accounts[key]:
+    #     print(accounts[key] [embedded_keys])
+
+# for acc_dictionary in accounts.values():
+    # print(acc_dictionary)
+    # for embedded_value in acc_dictionary.values():
+    #     print(embedded_value)
