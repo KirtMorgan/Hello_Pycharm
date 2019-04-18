a02c327dcfb90c8b1526b10f93a5b4caed2f6259
Kirt Morgan <kirtmorgan@live.com>
2019-04-18 16:06:15 +0100

    Finished work on bread factory function practice, and first practice with TDD

--- bread_factory.py
+++ bread_factory.py
@@ -1,6 +1,32 @@
 def dough(water, wheat):
-    print(f'Mixing {water} with {wheat}')
+    # print(f'Mixing {water} with {wheat}')
     if water == 'water' and wheat == 'wheat':
-        return'Dough'
+        return'dough'
     else:
         return'Blob of mixture'
+
+# TDD
+# output = (dough('water', 'wheat') == 'dough')
+# print(dough('water', 'wheat') == 'dough')
+
+def bake(substance):
+    if substance == 'dough':
+        return'bread'
+    else:
+        return'not bread'
+# TDD
+print(bake('dough')=='bread')
+
+def bread_factory(substance_1, substance_2):
+    output_dough = dough(substance_1, substance_2)
+    return bake(output_dough)
+
+# Test
+# These tests will test the bread factory as a whole
+print('testing the factory with water and wheat - expect output to be bread')
+print(bread_factory('water', 'wheat') =='bread')
+print(bread_factory('water', 'wheat'))
+
+print('testing the factory with water and cement - expect output to be not bread')
+print(bread_factory('water', 'cement') =='bread')
+print(bread_factory('water', 'cement'))
