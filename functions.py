6b266f969dddd5ef99414bb6f56b6de7ec90ff06
Kirt Morgan <kirtmorgan@live.com>
2019-04-18 15:12:24 +0100

    More work on functions

---
+++ functions.py
@@ -0,0 +1,24 @@
+# Functions only one job
+# Functions are like machines, they can take in inputs (arguments)
+# They run a block of code
+# Then output something or nothing
+
+# Syntax
+# Definition name (argument):
+# def making_bread ():
+#     print('Yummy Bread')
+#
+# making_bread()
+
+# def full_name(f_name, l_name):
+#     print(f_name, l_name)
+#
+# user_input1 = input('First name?')
+# user_input2 = input('Last name?')
+#
+# full_name(user_input1, user_input2)
+
+# Defining PI with a function
+# def circle_area(radius, pi=3.14):
+#     return pi*radius*radius
+# print(circle_area(10, 3.14))
