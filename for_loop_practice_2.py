6f789caaaa0f639fab544ac62a64474da8fd17f5
Kirt Morgan <kirtmorgan@live.com>
2019-04-18 12:13:28 +0100

    Uploading further work on python, more practice exercises and coding notes

--- for_loop_practice_2.py
+++ for_loop_practice_2.py
@@ -2,3 +2,17 @@ movies_places = {
     'movies_list':['Iron Man', 'Avengers', 'Real Steel', 'Ghost Busters', 'Big'],
     'places_list':['Japan', 'Russia', 'New York', 'New Vegas', 'Cornwall']
 }
+# for list in movies_places.values():
+#     print(list)
+#     for movie in list:
+#         print(movie)
+
+print('Here is my movie list:')
+for movie in movies_places['movies_list']:
+    print(movie)
+
+print()
+
+print('Here is a list of places i want to visit:')
+for place in movies_places['places_list']:
+    print(place)
--- for_loops.py
+++ for_loops.py
@@ -2,9 +2,11 @@
 # For place_holder in list_object
 # execute this block of code
 # Id List
-# ids_list = [1, 2, 3, 4, 5]
-# for id in ids_list:
-#     print(id)
+# cars = ['Fiat 500', 'Ford Galaxy', 'Toyota Yaris', 'Golf GTI', 'Fiat Panda']
+#
+# for car in cars:
+#     print(cars[3])
+#     break
 
 # Accessing data within a list embedded FOR loops
 # embedded_data = [[1, 2, 3], [7, 12, 1]]
@@ -20,9 +22,9 @@
 #     3:{'Name': 'Sponge','Money':'£500000'}
 # }
 # for key in accounts:
-    # print(accounts[key])
-    # for embedded_keys in accounts[key]:
-    #     print(accounts[key] [embedded_keys])
+#     print(accounts[key])
+#     for embedded_keys in accounts[key]:
+#         print(accounts[key] [embedded_keys])
 
 # for acc_dictionary in accounts.values():
     # print(acc_dictionary)
@@ -39,3 +41,19 @@
 #        print('too far! past 3')
 #    else:
 #        print('too soon')
+
+#Dictionaries & For loops
+
+# library_returns = {1:{'name': 'Richard', 'books': 3},
+#                    2:{'name': 'Joana', 'books': 3},
+#                    3:{'name': 'Brad', 'books': 3}
+#                    }
+# print(library_returns.keys())
+# for accounts in library_returns.values():
+#     for value in accounts.values():
+#         print(value)
+#         print('Inside loop')
+#     print('Loop Over')
+
+
+
